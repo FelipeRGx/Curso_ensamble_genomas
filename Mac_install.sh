326f117dab5a75b02c1b34f5261206f6961e9afe
felipe-omica <93945800+felipe-omica@users.noreply.github.com>
2024-09-09 19:40:59 -0700

    Update Mac_install.sh

--- Mac_install.sh
+++ Mac_install.sh
@@ -190,6 +190,7 @@ cd
 # Instalación de gdown para descargas grandes desde Google Drive
 # ----------------------------------------------
 echo -n "Instalando gdown	  ---->	"
+sudo apt-get install python3-pip -y
 (pip3 install gdown > /dev/null 2>&1) & spinner
 check_success "gdown"
 
