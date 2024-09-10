64fd7975e9591892bbb4c356c7091a79b8c7e7a0
felipe-omica <93945800+felipe-omica@users.noreply.github.com>
2024-09-09 18:28:04 -0700

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -187,7 +187,7 @@ gdown  151PeMSeGnQJstXOvMOn8ArbbG49JIXes -O $DATA_DIR/reference.fasta
 # Descomprimir el archivo ZIP
 # ----------------------------------------------
 echo "Descomprimiendo archivo ZIP	  ---->	"
-unzip $DATA_DIR/archivo1.zip -d $DATA_DIR
+sudo tar -xvzf $DATA_DIR/fastq.zip -C $DATA_DIR
 
 # ----------------------------------------------
 # Resumen de la instalación
